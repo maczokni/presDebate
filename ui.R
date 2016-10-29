1c36dd09dbdde3175b7e253224439bf42399f10f
maczokni <maczokni@gmail.com>
2016-10-29 19:28:28 +0100

    typo fix

--- ui.R
+++ ui.R
@@ -2,7 +2,7 @@ shinyUI(pageWithSidebar(
   headerPanel('2016 US Presidential Debates Analyser'),
   sidebarPanel(
     h3("Select a word to find out which candidate said it more, and to find associated words."),
-    p("Candidates put frwarf a number of isses they hope to represent, for example women's rights and job creation.
+    p("Candidates put forward a number of isses they hope to represent, for example women's rights and job creation.
        You can use this app to search some words related to these issues, to see which candidate said them more often.
        Also, you can explore the context in which candidates used these words, but seeing a list of associated words as well."),
     h4("Type the word you're interested in below: "),
